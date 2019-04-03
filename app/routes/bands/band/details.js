c788caa0fe8ca26e6d9f5fa39beb019c71872e76
Alex Cich <cichal13@gmail.com>
2019-04-03 10:00:01 -0600

    added routing for artist details

--- app/routes/bands/band/details.js
+++ app/routes/bands/band/details.js
@@ -1,7 +1,7 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
-  beforeModel() {
-    this.transitionTo('bands');
+  model() {
+    return this.modelFor('bands.band');
   }
 });
