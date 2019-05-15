8b6bddd7fd9657b00fecebb35b35981e5a563d4c
Alex Cich <cichal13@gmail.com>
2019-05-15 08:55:12 -0600

    completed chapter 9

--- app/routes/bands/band.js
+++ app/routes/bands/band.js
@@ -1,9 +1,8 @@
 import Route from '@ember/routing/route';
-import { isEmpty } from '@ember/utils';
+//import { isEmpty } from '@ember/utils'; removed for now since we import but do nothing with it
 
 export default Route.extend({
   model(params) {
-    let bands = this.modelFor('bands');
-    return bands.findBy('slug', params.slug); // params.slug is now 'pearl-jam'
-  },
+    return this.store.findRecord('band', params.id);
+  }
 });
